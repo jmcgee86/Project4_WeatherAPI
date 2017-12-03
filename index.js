a0acde685e29e5c0c1296ace938cfeeb83ace35f
Jim McGee <jmcgee86@gmail.com>
2017-12-03 22:35:05 +0000

    fix bugs, add style

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 /* global $ APIKEY navigator*/
 
 $(document).ready(function() {
-	
-	
   
-  var failed = document.getElementById("fail");
+  var fail = document.getElementById("failed");
 
   if (!navigator.geolocation){
-    failed.style.visibility = "visible";
+    fail.style.visibility = "visible";
     console.log("no geo location");
     return;
   }
@@ -18,7 +16,7 @@ $(document).ready(function() {
 	
 	$.ajax({
 		method: "GET",
-		url: "https://api.openweathermap.org/data/2.5/forecast?",
+		url: "https://api.openweathermap.org/data/2.5/weather",
 		data: {
 			lat: latitude,
 			lon: longitude,
@@ -27,51 +25,44 @@ $(document).ready(function() {
 		success: function(data) {
 			//if (data.status === "ok") {
 				console.log(data);
-				var weatherIcon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
-				document.getElementById("icon").innerHTML = '<img src = "' + weatherIcon + '">';
 				
-				function fConverter(valNum1,valNum2, valNum3) {
+				function fConverter(valNum1) {
 				document.getElementById("temp").innerHTML = Math.round(((valNum1-273.15)*1.8)+32) + "°F";
-				document.getElementById("min").innerHTML = Math.round(((valNum2-273.15)*1.8)+32) + "°F";
-				document.getElementById("max").innerHTML = Math.round(((valNum3-273.15)*1.8)+32) + "°F";
 				}
 				
-				fConverter (data.list[0].main.temp, data.list[0].main.temp_min, data.list[0].main.temp_max);
+				fConverter (data.main.temp);
 
-				function cConverter(valNum1, valNum2, valNum3){
+				function cConverter(valNum1){
 				 document.getElementById("temp").innerHTML= Math.round (valNum1-273.15) + "C°";
-				 document.getElementById("min").innerHTML= Math.round (valNum2-273.15) + "C°";
-				 document.getElementById("max").innerHTML= Math.round (valNum3-273.15) + "C°";
 				}
+				
+				var weatherIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+				document.getElementById("icon").innerHTML = '<img src = "' + weatherIcon + '">';
+				document.getElementById("description").innerHTML = data.weather[0].description;
+				document.getElementById("city").innerHTML = data.name;
 				
 				$('#changetemp').click(function() {
 					if (document.getElementById("changetemp").innerHTML === "F°"){
-						cConverter(data.list[0].main.temp, data.list[0].main.temp_min, data.list[0].main.temp_max);
+						cConverter(data.main.temp);
 						document.getElementById("changetemp").innerHTML = "C°";
 						return;
 					}
 					else {
-						fConverter(data.list[0].main.temp, data.list[0].main.temp_min, data.list[0].main.temp_max);
+						fConverter(data.main.temp);
 						document.getElementById("changetemp").innerHTML = "F°";
 					}
+					
 	});	
-				
-				//var tempf = fConverter(data.list[0].main.temp);
-				//var tempfmin  = fConverter(data.list[0].main.temp_min);
-				//var tempfmax = fConverter(data.list[0].main.temp_max);
-				//document.getElementById("temp").innerHTML = tempf + "°F";
-				//document.getElementById("min").innerHTML = tempfmin +"°F";
-				//document.getElementById("max").innerHTML = tempfmax +"°F";
-				
-				//cConverter(data.list[0].main.temp, data.list[0].main.temp_min, data.list[0].main.temp_max);
+
 			}
 			
 			
 	});
-	
+  	
   }
+  
   function error() {
-    failed.style.visibility = "visible";
+    fail.style.visibility = "visible";
     console.log("fail");
     var zipcode = document.getElementById("failed").value; +",US";
  
@@ -81,13 +72,13 @@ $(document).ready(function() {
     	var zipcode = document.getElementById("zc").value; +",US";
     	$.ajax({
 		method: "GET",
-		url: "https://api.openweathermap.org/data/2.5/forecast?",
+		url: "https://api.openweathermap.org/data/2.5/weather",
 		data: {
 			zip: zipcode, 
 			appid: APIKEY
 		},
 		success: function(data) {
-			//if (data.status === "ok") {
+
 				console.log(data);
 			}
     	
@@ -95,21 +86,43 @@ $(document).ready(function() {
     
 	});
   }
-  //}
-  navigator.geolocation.getCurrentPosition(success, error);
+ 
+ navigator.geolocation.getCurrentPosition(success, error);
   
   
 });
 
 
-/*$('#changetemp').click(function() {
-					if (document.getElementById("changetemp").innerHTML === "F°"){
-						cConverter;
-						document.getElementById("changetemp").innerHTML = "C°";
-						return;
+
+/*
+change background img base on id - not currently working
+				
+				function changeTheme(){
+					console.log("changeimg");
+					if (data.weather[0].id === 804){
+						document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1469282311538-c8b591f67d2e?auto=format&fit=crop&w=1951&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//cloudy
 					}
-					else {
-						fConverter;
-						document.getElementById("changetemp").innerHTML = "F°";
-					}
-	});	*/
+				if (200 <= data.weather[0].id < 300 ){
+					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1506432734318-4bf212257692?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//thunder
+				}
+				else if (300 <= data.weather[0].id < 400 ){
+					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1498946765439-9c2480c63e19?auto=format&fit=crop&w=934&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//rain
+				}
+				else if (500 <= data.weather[0].id < 600 ){
+					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1498946765439-9c2480c63e19?auto=format&fit=crop&w=934&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//rain
+				}
+				else if (600 <= data.weather[0].id < 700 ){
+					document.getElementById("weatherdisplay").style.backgroundImage = "url('https://images.unsplash.com/photo-1483719728008-b73ef3e9956b?auto=format&fit=crop&w=934&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//snow
+				}else if (data.weather[0].id === 800 ){
+					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1500382017468-9049fed747ef?auto=format&fit=crop&w=2689&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//nice
+				}
+				else if (801 <= data.weather[0].id < 810 ){
+					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1469282311538-c8b591f67d2e?auto=format&fit=crop&w=1951&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//cloudy
+				}
+				else if (900 <= data.weather[0].id < 910 ){
+					document.getElementById("bg").innerHTML = "";//extreme
+				}
+				}
+changeTheme();
+
+*/
