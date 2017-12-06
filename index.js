6a40ab3c73c7ffda92b92f43639faec5ce697c90
Jim McGee <jmcgee86@gmail.com>
2017-12-06 00:02:36 +0000

    if geolocation fails, option to enter zipcode appears

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
   var fail = document.getElementById("failed");
 
   if (!navigator.geolocation){
-    fail.style.visibility = "visible";
+    //fail.style.visibility = "visible";
     console.log("no geo location");
+    error();
     return;
   }
 
@@ -106,51 +107,77 @@ warning();
 			appid: APIKEY
 		},
 		success: function(data) {
-
 				console.log(data);
+				
+				function fConverter(valNum1) {
+				document.getElementById("temp").innerHTML = Math.round(((valNum1-273.15)*1.8)+32) + "°F";
+				}
+				
+				fConverter (data.main.temp);
+
+				function cConverter(valNum1){
+				 document.getElementById("temp").innerHTML= Math.round(valNum1-273.15) + "°C";
+				}
+				
+				var weatherIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+				document.getElementById("icon").innerHTML = '<img src = "' + weatherIcon + '">';
+				document.getElementById("description").innerHTML = data.weather[0].description;
+				document.getElementById("city").innerHTML = data.name;
+				
+				$('#changetemp').click(function() {
+					if (document.getElementById("changetemp").innerHTML === "F°"){
+						cConverter(data.main.temp);
+						document.getElementById("changetemp").innerHTML = "C°";
+						return;
+					}
+					else {
+						fConverter(data.main.temp);
+						document.getElementById("changetemp").innerHTML = "F°";
+					}
+					
+	});	
+					function changeTheme(){
+					if (data.weather[0].main == "Clouds"){//clouds
+						document.getElementById("displayimg").innerHTML = "<img src = 'https://images.pexels.com/photos/158163/clouds-cloudporn-weather-lookup-158163.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb'>";
+					}
+					else if (data.weather[0].main == "Rain"){//rain
+						document.getElementById("displayimg").innerHTML = "<img src = 'https://images.pexels.com/photos/110874/pexels-photo-110874.jpeg?w=260&h=260&dpr=2&auto=compress&cs=tinysrgb'>";
+					}
+					else if (data.weather[0].main == "Snow"){//snow
+					document.getElementById("displayimg").innerHTML = "<img src = 'https://images.pexels.com/photos/60561/winter-snow-nature-60561.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb'>";
+				}
+				else if (data.weather[0].main == "Clear"){//clear
+					document.getElementById("displayimg").innerHTML = "<img src = 'https://images.pexels.com/photos/3768/sky-sunny-clouds-cloudy.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb'>"
+				}
+				else if (data.weather[0].main == "Mist"){//mist
+					document.getElementById("displayimg").innerHTML = "<img src = 'https://images.pexels.com/photos/5230/road-fog-foggy-mist.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb'>"
+				}
+				else if (data.weather[0].main == "Thunder"){//thunder
+					document.getElementById("displayimg").innerHTML = "<img src = 'https://static.pexels.com/photos/53459/lightning-storm-weather-sky-53459.jpeg'>"
+				}
+		}
+		
+		function warning(){
+			console.log("warning check");
+			if (data.weather[0].main == "Extreme"){
+				document.getElementById("warn").innerHTML = '<h3>**Warning, extreme weather expected in your area.**</h3>';
 			}
+		}
+changeTheme();
+warning();
+			}
+			
+			
+	//});
+    
+	//		}
     	
     	});
     
 	});
   }
  
- navigator.geolocation.getCurrentPosition(success, error);
+ navigator.geolocation.getCurrentPosition(success,error);
   
   
 });
-
-
-
-
-//change background img base on id - not currently working
-	/*			
-				function changeTheme(){
-					console.log("changeimg");
-					if (data.weather[0].main == "clear"){
-						document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1469282311538-c8b591f67d2e?auto=format&fit=crop&w=1951&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//cloudy
-					}
-
-				/*if (200 <= data.weather[0].id < 300 ){
-					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1506432734318-4bf212257692?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//thunder
-				}
-				else if (300 <= data.weather[0].id < 400 ){
-					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1498946765439-9c2480c63e19?auto=format&fit=crop&w=934&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//rain
-				}
-				else if (500 <= data.weather[0].id < 600 ){
-					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1498946765439-9c2480c63e19?auto=format&fit=crop&w=934&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//rain
-				}
-				else if (600 <= data.weather[0].id < 700 ){
-					document.getElementById("weatherdisplay").style.backgroundImage = "url('https://images.unsplash.com/photo-1483719728008-b73ef3e9956b?auto=format&fit=crop&w=934&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//snow
-				}else if (data.weather[0].id === 800 ){
-					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1500382017468-9049fed747ef?auto=format&fit=crop&w=2689&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//nice
-				}
-				else if (801 <= data.weather[0].id < 810 ){
-					document.getElementById("displayimg").style.backgroundImage = "url('https://images.unsplash.com/photo-1469282311538-c8b591f67d2e?auto=format&fit=crop&w=1951&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')";//cloudy
-				}
-				else if (900 <= data.weather[0].id < 910 ){
-					document.getElementById("bg").innerHTML = "";//extreme
-				}
-				}
-changeTheme();
-*/
